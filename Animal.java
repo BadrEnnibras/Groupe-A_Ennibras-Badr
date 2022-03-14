e028dfd7af552fcf7a743d988578ec0e024574a3
Badr Ennibras <bennibras@SMB.SQLI.COM>
2022-03-14 15:20:25 +0100

    Add the default response correctly

--- Animal.java
+++ Animal.java
@@ -1,12 +1,16 @@
 public class Animal {
-    public static String DefaultAnimal(){
-
+    public static String DefaultAnimal() {
         System.out.println("Hello");
     }
 
     public static void main(String[] args) {
         String firstArguments = args[0];
         String output = "";
-        System.out.println(DefaultAnimal);
+
+        if (firstArguments.equalsIgnoreCase("bird")) {
+            output = getCatGreeting();
+        } else {
+            System.out.println(DefaultAnimal);
+        }
     }
 }
