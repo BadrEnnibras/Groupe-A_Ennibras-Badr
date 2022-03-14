b3490905711f67eea4dc15cd12710766c951d937
Badr Ennibras <bennibras@SMB.SQLI.COM>
2022-03-14 15:12:13 +0100

    Add the default response

---
+++ Animal.java
@@ -0,0 +1,9 @@
+public class Animal {
+    public static String DefaultAnimal(){
+        System.out.println("Hello");
+    }
+
+    public static void main(String[] args) {
+        System.out.println(DefaultAnimal);
+    }
+}
