85b2493c1934666fa29d6dadbb588f40b649389c
Badr Ennibras <bennibras@SMB.SQLI.COM>
2022-03-14 15:15:15 +0100

    Add the default response correctly

--- Animal.java
+++ Animal.java
@@ -1,9 +1,12 @@
 public class Animal {
     public static String DefaultAnimal(){
+
         System.out.println("Hello");
     }
 
     public static void main(String[] args) {
+        String firstArguments = args[0];
+        String output = "";
         System.out.println(DefaultAnimal);
     }
 }
